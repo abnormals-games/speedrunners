07ed7c3cb7aef17cc85afa71787827c90dda98a1
JacksonPlayz <jackson.hardaway@gmail.com>
2020-09-02 23:06:09 -0700

    Improved scoreboard sidebar

--- src/main/java/me/jaackson/speedrunners/game/manager/ScoreboardManager.java
+++ src/main/java/me/jaackson/speedrunners/game/manager/ScoreboardManager.java
@@ -1,16 +1,21 @@
 package me.jaackson.speedrunners.game.manager;
 
 import me.jaackson.speedrunners.SpeedRunners;
-import net.minecraft.scoreboard.*;
+import net.minecraft.scoreboard.Score;
+import net.minecraft.scoreboard.ScoreCriteria;
+import net.minecraft.scoreboard.ScoreObjective;
+import net.minecraft.scoreboard.ServerScoreboard;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 
 public class ScoreboardManager {
+    public static ScoreObjective infoObjective;
+
 
     public static void createScoreboard() {
         ServerScoreboard scoreboard = SpeedRunners.getTeamManager().getScoreboard();
         ScoreObjective objective = scoreboard.getObjective("sr_info");
-        if(objective == null) {
+        if (objective == null) {
             ScoreObjective infoObjective = scoreboard.addObjective(
                     "sr_info",
                     ScoreCriteria.DUMMY,
@@ -18,19 +23,23 @@ public class ScoreboardManager {
                     ScoreCriteria.RenderType.INTEGER
             );
             scoreboard.setObjectiveInDisplaySlot(1, infoObjective);
-            updateSidebar(scoreboard, infoObjective, new String[]{
-                    TextFormatting.RED + "H " + TextFormatting.RESET + "Hunters" + TextFormatting.DARK_GRAY + "»" + TextFormatting.RESET + " 0",
-                    TextFormatting.AQUA + "R " + TextFormatting.RESET + "Runners" + TextFormatting.DARK_GRAY + " » " + TextFormatting.RESET + "0"
-            });
+            ScoreboardManager.infoObjective = infoObjective;
         } else {
-            updateSidebar(scoreboard, objective, new String[]{
-                    TextFormatting.RED + "H " + TextFormatting.RESET + "Hunters" + TextFormatting.DARK_GRAY + "»" + TextFormatting.RESET + " 0",
-                    TextFormatting.AQUA + "R " + TextFormatting.RESET + "Runners" + TextFormatting.DARK_GRAY + " » " + TextFormatting.RESET + "0"
-            });
+            ScoreboardManager.infoObjective = objective;
         }
+        
+        updateSidebar(scoreboard, infoObjective, new String[]{
+                "  ",
+                "Time Elapsed: " + TextFormatting.GRAY + "1:00",
+                "",
+                TextFormatting.RED + "H " + TextFormatting.RESET + "Hunters: " + TextFormatting.GRAY + "0",
+                TextFormatting.AQUA + "R " + TextFormatting.RESET + "Runners: " + TextFormatting.GRAY + "0",
+                " ",
+                TextFormatting.GRAY + "games.minecraftabnormals.com"
+        });
     }
 
-    private static void updateSidebar(ServerScoreboard scoreboard, ScoreObjective objective, String[] lines) {
+    public static void updateSidebar(ServerScoreboard scoreboard, ScoreObjective objective, String[] lines) {
         for (Score score : scoreboard.getSortedScores(objective))
             scoreboard.removeObjectiveFromEntity(score.getPlayerName(), objective);
         for (int i = 0; i < lines.length; i++) {
